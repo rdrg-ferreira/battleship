c6f5bc3b663bd85e6dac0d68e55b7fd1c9c34948
Rodrigo Ferreira <rodrigo.slva.ferreira@gmail.com>
2026-02-03 18:10:34 +0000

    added ship class and its tests

---
+++ src/modules/ship.js
@@ -0,0 +1,18 @@
+class Ship {
+    constructor(length) {
+        this.length = length;
+        this.hitCount = 0;
+        this.hasSunk = false;
+    }
+
+    hit() {
+        this.hitCount++;
+        this.isSunk();
+    }
+
+    isSunk() {
+        this.hasSunk = this.length === this.hitCount;
+    }
+}
+
+module.exports = Ship;
---
+++ tests/ship.test.js
@@ -0,0 +1,15 @@
+const Ship = require("../src/modules/ship");
+
+test("hit count increaments correctly", () => {
+    const s = new Ship(3);
+    s.hit();
+    expect(s.hitCount).toBe(1);
+});
+
+test("isSunk determines the ship has sunk correctly", () => {
+    const s = new Ship(1);
+    expect(s.hasSunk).toBeFalsy();
+
+    s.hit();
+    expect(s.hasSunk).toBeTruthy();
+});
